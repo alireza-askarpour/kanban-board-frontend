76a2f1e531754682698067212b6ce959ff19c6b3
Alireza Askarpour <askarpourdev@gmail.com>
2022-08-24 14:02:18 -0400

    feat: add SEO component

---
+++ components/Shared/SEO/SEO.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head"
+import settings from "./settings"
+import { IProps } from "types/components/SEO"
+
+const SEO = ({ title, description }: IProps) => {
+  return (
+    <Head>
+      <title> | {title}</title>
+      <meta name="description" content={description} />
+    </Head>
+  )
+}
+
+SEO.defaultProps = {
+  title: settings.meta.title,
+  description: settings.meta.description
+}
+
+export default SEO
---
+++ components/Shared/SEO/settings.ts
@@ -0,0 +1,8 @@
+const settings = {
+  meta: {
+    title: "Notion",
+    description: "Notion is the ultimate project management tool. Start up a board in seconds, automate tedious tasks",
+  },
+}
+
+export default settings
