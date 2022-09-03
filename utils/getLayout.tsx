97910cad5fdc4012c35db1edf7e946059fd8cd98
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-03 06:50:09 -0700

    feat: add getLayout utils

---
+++ utils/getLayout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+import { authRoutes } from "constants/routes"
+
+import AuthLayout from "components/Layouts/AuthLayout"
+import MainLayout from "components/Layouts/MainLayout"
+
+const getLayout = (pathname: string, children: ReactNode): JSX.Element => {
+  if (authRoutes.includes(pathname)) return <AuthLayout children={children} />
+  else if (pathname === "/") return <>{children}</>
+  else return <MainLayout children={children} />
+}
+
+export default getLayout
--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,2 @@
-export { classNames } from './classNames'
+export { classNames } from "./classNames"
+export { default as getLayout } from "./getLayout"
