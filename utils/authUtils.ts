2a6cc9081f6084e037a0fa96d3405aa463e3f656
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-05 12:07:59 -0700

    feat: add auth utils

---
+++ utils/authUtils.ts
@@ -0,0 +1,14 @@
+import * as accountService from "services/account.service"
+
+const isAuthenticated = async () => {
+  const token = localStorage.getItem("token")
+  if (!token) return false
+
+  const res = await accountService.verifyToken()
+  if (res.success) return res.user
+  return false
+}
+
+export const authUtils = {
+  isAuthenticated,
+}
