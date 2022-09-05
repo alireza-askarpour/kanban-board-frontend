46633d67dad9b98acef0ec69d7f21f18e9089b39
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-05 11:52:35 -0700

    feat: add auth services

---
+++ services/account.service.ts
@@ -0,0 +1,28 @@
+import http from "config/http"
+
+export const loginUser = async (user: { email: string, password: string }) => {
+  try {
+    const { data } = await http.post("/auth/login", user)
+    return { success: true, token: data.token }
+  } catch (err) {
+    return { success: false, error: err }
+  }
+}
+
+export const signUpUser = async (user: { fullname: string; password: string; username: string; email: string }) => {
+  try {
+    const { data } = await http.post("/auth/signup", user)
+    return { success: true, token: data.token }
+  } catch (err) {
+    return { success: false, error: err }
+  }
+}
+
+export const verifyToken = async () => {
+  try {
+    const { data } = await http.post("/auth/verify-token")
+    return { success: true, user: data.user }
+  } catch (err) {
+    return { success: false, error: err }
+  }
+}
