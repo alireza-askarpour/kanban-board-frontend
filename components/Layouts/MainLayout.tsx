1479521b028561939d65343a6572b93c85ca6748
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-13 04:34:29 -0700

    feat: add loading in main layout

--- components/Layouts/MainLayout.tsx
+++ components/Layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 
 import { useAccount } from "providers/Account/AccountProvider"
@@ -7,25 +7,31 @@ import Sidebar from "./Sidebar"
 
 const MainLayout = ({ children }) => {
   const router = useRouter()
+  const [loading, setLoading] = useState(true)
+
   const { handleSetUser } = useAccount()
 
   useEffect(() => {
     const checkAuth = async () => {
       const user = await authUtils.isAuthenticated()
       if (!user) {
+        setLoading(false)
         router.push("/login")
       } else {
+        setLoading(false)
         handleSetUser(user)
       }
     }
     checkAuth()
   }, [])
 
-  return (
+  return loading ? (
+    <div>Loading...</div>
+  ) : (
     <div>
       <Sidebar />
       <div className="max-h-screen overflow-y-auto ml-64 flex-1">
-        <div>{children}</div>
+        <div className="min-h-screen">{children}</div>
       </div>
     </div>
   )
