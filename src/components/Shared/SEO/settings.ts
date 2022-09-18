fcd21cdbd87ba0a141529effca8df8a85385fdff
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-18 06:44:22 -0700

    delete: remove SEO component

--- src/components/Shared/SEO/settings.ts
+++
@@ -1,8 +0,0 @@
-const settings = {
-  meta: {
-    title: "Notion",
-    description: "Notion is the ultimate project management tool. Start up a board in seconds, automate tedious tasks",
-  },
-}
-
-export default settings
