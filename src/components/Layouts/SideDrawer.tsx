99d23a94b53902f1541181f5404d5a270e8af282
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-19 12:58:00 -0700

    feat: add side drawer components

---
+++ src/components/Layouts/SideDrawer.tsx
@@ -0,0 +1,27 @@
+import { classNames } from "utils/classNames"
+import Sidebar from "./Sidebar"
+
+interface IProps {
+  show: boolean
+  hideMenu: () => void
+}
+
+const SideDrawer = ({ show, hideMenu }: IProps) => {
+  return (
+    <section
+      className={classNames("w-full h-screen transition-all fixed inset-0 z-40 xl:hidden", !show && "invisible")}
+    >
+      <section
+        className={classNames(
+          "w-64 h-screen transition-all duration-300 fixed top-0 bottom-0 z-20",
+          show ? "left-0" : "-left-full",
+        )}
+      >
+        <Sidebar sideDrawer />
+      </section>
+      <section className="w-full h-screen fixed inset-0 bg-[#22292f80]" onClick={hideMenu}></section>
+    </section>
+  )
+}
+
+export default SideDrawer
