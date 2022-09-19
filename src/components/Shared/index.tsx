67de4edb284c08243ec071b0b3a0f0d16a765f26
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-19 04:43:34 -0700

    feat: add multiple import for shared(reusable) components

---
+++ src/components/Shared/index.tsx
@@ -0,0 +1,6 @@
+export { default as Button } from "./Button/Button"
+export { default as Input } from "./Input/Input"
+export { default as Icon } from "./Icon/Icon"
+export { default as EmojiPicker } from "./EmojiPicker/EmojiPicker"
+export { default as Modal } from "./Modal/Modal"
+export { default as Helmet } from "./Helmet/Helmet"
