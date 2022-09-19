c0b315648e9014495a998dc8d2147e8efdf2fa41
Alireza Askarpour <askarpourdev@gmail.com>
2022-09-19 04:40:39 -0700

    feat: add helmet component

---
+++ src/components/Shared/Helmet/Helmet.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+import { IProps } from "types/components/Helmet"
+
+const Helmet = ({ title, children }: IProps) => {
+  document.title = "Kanban | " + title
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
+  return <div>{children}</div>
+}
+
+export default Helmet
---
+++ src/types/components/Helmet.ts
@@ -0,0 +1,4 @@
+export interface IProps {
+  title: string
+  children: React.ReactNode
+}
