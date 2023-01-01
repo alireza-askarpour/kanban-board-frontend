48ba9974b8eeddfb7a5c55bd07dd94e09b378d2f
Alireza Askarpour <askarpourdev@gmail.com>
2023-01-01 14:52:14 +0330

    feat: add className and fontSize props in avatar component

--- src/components/Shared/Avatar/Avatar.tsx
+++ src/components/Shared/Avatar/Avatar.tsx
@@ -1,20 +1,22 @@
 import { classNames } from "utils"
 import { IProps } from "types/components/Avatar"
 
-const Avatar = ({ variant, size, children, src, alt }: IProps) => {
+const Avatar = ({ variant, size, children, src, alt, className, fontSize }: IProps) => {
   return (
     <div
       className={classNames(
-        "bg-gray-200 text-base grid place-content-center overflow-hidden",
+        "bg-[#37352f17] grid place-content-center overflow-hidden",
         variant === "circle" && "rounded-full",
         variant === "square" && "rounded-[4px]",
+        className
       )}
       style={{
         width: size || "24px",
         height: size || "24px",
+        fontSize: `${fontSize}px` || "16px"
       }}
     >
-      {!src && children && <span className="text-gray-600">{children}</span>}
+      {!src && children && <span className="text-gray-500">{children}</span>}
       {!children && <img className="w-6 h-6 object-cover" src={src} alt={alt} />}
     </div>
   )
