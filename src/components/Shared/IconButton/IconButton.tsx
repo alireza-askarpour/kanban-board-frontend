562e013f74fc52c5e8006c01f8993f90392b697e
Alireza Askarpour <askarpourdev@gmail.com>
2023-01-25 16:12:33 +0330

    feat: add icon button component

---
+++ src/components/Shared/IconButton/IconButton.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom"
+import { IProps } from "types/components/IconButton"
+import { classNames } from "utils/classNames"
+
+const IconButton = (props: IProps) => {
+  const size = props.size || "md"
+
+  const handleClickButton = () => {
+    if (props.disabled) return
+    if (typeof props.onClick === "function") props.onClick()
+  }
+
+  const elementProps = {
+    onClick: handleClickButton,
+    type: props.type || "button",
+    disabled: props.disabled,
+    style: props.style,
+    className: classNames(
+      props.className,
+      "border border-transparent",
+      "grid place-items-center",
+      "rounded-full cursor-pointer select-none",
+      "transition duration-150 ease-in-out",
+      "appearance-none focus:outline-none bg-[#F7F7F7]",
+
+      props.disabled && "cursor-not-allowed opacity-50 hover:opacity-50",
+
+      size === "lg" && "w-12 h-12",
+      size === "md" && "w-10 h-10",
+      size === "sm" && "w-8 h-8",
+    ),
+  }
+
+  return props.href ? (
+    <Link to={props.href} style={{ appearance: "none" }} {...elementProps}>
+      {props.children}
+    </Link>
+  ) : (
+    <button {...elementProps}>{props.children}</button>
+  )
+}
+
+export default IconButton
--- src/types/components/Icon.ts
+++ src/types/components/Icon.ts
@@ -1,9 +1,11 @@
 import { IconName } from "components/Shared/Icon/icons"
 
+type colors = "black" | "blue" | "gray" | "green" | "orange" | "purple" | "red" | "white"
+
 export interface IIcon {
   size?: number
   className?: string
-  variant?: "outline" | "bold"
-  color?: "black" | "blue" | "gray" | "green" | "orange" | "purple" | "red" | "white"
+  fill?: colors
+  stroke?: colors
   name: IconName
 }
