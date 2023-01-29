d0d094aace0dd213c51acdf5c012f3f35874233a
Alireza Askarpour <askarpourdev@gmail.com>
2023-01-29 21:29:58 +0330

    feat: add breadcrumbs component

---
+++ src/components/Shared/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,11 @@
+const Breadcrumbs = () => {
+  return (
+    <div className="flex text-sm text-[#37352f] select-none">
+      <div className="px-1.5 leading-6 h-6 rounded-[4px] cursor-pointer transition-colors hover:bg-[#efefef]">Breadcrumbs</div>
+      <span className="mx-0.5 leading-6 text-[#37352f80]">/</span>
+      <div className="px-1.5 leading-6 h-6 rounded-[4px] cursor-pointer transition-colors hover:bg-[#efefef]">Breadcrumbs</div>
+    </div>
+  )
+}
+
+export default Breadcrumbs
