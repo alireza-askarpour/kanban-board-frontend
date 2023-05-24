063ce08f0089c13704b5f94900dceaace348b4de
Alireza Askarpour <askarpourdev@gmail.com>
2023-05-24 09:34:25 -0400

    refactor: add add rounded prop and refactor varient prop

--- src/components/Shared/Avatar/Avatar.tsx
+++ src/components/Shared/Avatar/Avatar.tsx
@@ -1,22 +1,38 @@
 import { classNames } from "../../../utils"
 import { IProps } from "../../../types/components/Avatar"
 
-const Avatar = ({ variant, size, children, src, alt, className, fontSize }: IProps) => {
+const Avatar = ({ variant = "primary", size, children, src, alt, className, fontSize, rounded }: IProps) => {
   return (
     <div
       className={classNames(
-        "bg-[#37352f17] grid place-content-center overflow-hidden",
-        variant === "circle" && "rounded-full",
-        variant === "square" && "rounded-[4px]",
-        className
+        className,
+        "grid place-content-center overflow-hidden",
+
+        variant === "primary" && "bg-[#37352f17]",
+        variant === "secondary" && "bg-white border border-[#37352f]",
+
+        rounded === "none" && "rounded-none",
+        rounded === "sm" && "rounded-sm",
+        rounded === "md" && "rounded-md",
+        rounded === "lg" && "rounded-lg",
+        rounded === "xl" && "rounded-xl",
+        rounded === "2xl" && "rounded-2xl",
+        rounded === "3xl" && "rounded-3xl",
+        rounded === "full" && "rounded-full",
       )}
       style={{
         width: size || "24px",
         height: size || "24px",
-        fontSize: `${fontSize}px` || "16px"
+        fontSize: `${fontSize}px` || "16px",
       }}
     >
-      {!src && children && <span className="text-gray-500">{children}</span>}
+      {!src && children && (
+        <span
+          className={classNames(variant === "primary" && "text-gray-500", variant === "secondary" && "text-[#37352f]")}
+        >
+          {children}
+        </span>
+      )}
       {!children && <img className="w-6 h-6 object-cover" src={src} alt={alt} />}
     </div>
   )
--- src/types/components/Avatar.ts
+++ src/types/components/Avatar.ts
@@ -2,7 +2,8 @@ import { ReactNode } from "react"
 
 export interface IProps {
   className?: string
-  variant: "circle" | "square"
+  variant: "primary" | "secondary"
+  rounded?: "none" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "full"
   size: number
   children?: ReactNode
   src?: string
